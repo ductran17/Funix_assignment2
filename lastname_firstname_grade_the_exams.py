93fe0af66726353aee66afaff901161882d8c7f3
ductm27 <mrductran173@gmail.com>
2023-07-21 02:01:37 +0700

    finished Task 3

--- lastname_firstname_grade_the_exams.py
+++ lastname_firstname_grade_the_exams.py
@@ -18,12 +18,12 @@ print("**** ANALYZING ****")
 content = f.read()
 lines = re.split('\n', content)
 regex = 'N[0-9]{8}(?:,[A-D]?){25}$'
-answers = []
+id_answers = []
 faults = []
 for line in lines:
     match = re.match(regex, line)
     if match:
-        answers.append(line[10:])
+        id_answers.append(line)
     else:
         faults.append(line)
 if (len(faults) == 0):
@@ -48,26 +48,61 @@ print("Total invalid lines of data:", len(faults))
 
 answers_key = "B,A,D,D,C,B,D,A,C,C,D,B,A,B,A,C,B,D,A,C,A,A,B,D,D"
 answers_key = re.split(',', answers_key)
-marks = []
-for answer in answers:
-    i = 0
-    mark = 0
-    answer = re.split(',', answer)
-    while i < 25:
-        if answer[i] != '':
-            if answer[i] == answers_key[i]:
-                mark += 4
-            else:
-                mark -= 1
-        i += 1
-    marks.append(mark)
-marks = np.array(marks)
-print("Total student of high scores: ", round(marks[marks > 80], 3))
-print("Mean (average) score: ", round(marks.mean(), 3))
-print("Highest score: ", round(marks.max(), 3))
-print("Lowest score: ", round(marks.low(), 3))
-print("Range of scores: ", round(marks.max()-marks.min(), 3))
-print("Median score: ", round(marks.median(), 3))
+answers_key = np.array(answers_key)
+# Using pandas to turn all the answers into dataframes
+num_of_col = 25
+df = pd.DataFrame({f'question_{i+1}': []
+                   for i in range(num_of_col)})
+for e in id_answers:
+    e = re.split(',', e)
+    id = e[0]
+    answer = e[1:]
+    answer = np.array(answer)
+    point = (answer == answers_key)*4 + \
+        ((answer != '') & (answer != answers_key))*(-1)
+    df.loc[id] = point
+df['total_point'] = [df.loc[index].sum()
+                     for index in list(df.index)]
+print(df)
+print("Total student of high scores: ", len(df[df["total_point"] > 80]))
+print("Mean (average) score: ", df['total_point'].mean().round(3))
+print("Highest score: ", df["total_point"].max())
+print("Lowest score: ", df["total_point"].min())
+print("Range of scores: ", df['total_point'].max()-df['total_point'].min())
+print("Median score: ", df["total_point"].median())
+# TASK 3.7+3.8
+# create a series of all questions along with the number of students who dont know the answer for each question
+skip_ans = pd.Series()
+false_ans = pd.Series()
+for q in list(df.columns)[:-1]:
+    ques_stat = df[q].value_counts()
+    # if a question is skipped, it means the poin for it is 0
+    if 0 in list(ques_stat.index):
+        skip_ans[q] = ques_stat[0]
+    else:
+        skip_ans[q] = 0
+    # if a question is skipped, it means the poin for it is -1
+    if -1 in list(ques_stat.index):
+        false_ans[q] = ques_stat[-1]
+    else:
+        false_ans[q] = 0
 
-
-print(marks.mean())
+# print the most skipped questions
+skip_ans = skip_ans.sort_values(ascending=False)
+max_skip = skip_ans[skip_ans == skip_ans.max()]
+print("Question that most people skip: ", end="")
+for i in list(max_skip.index):
+    if i != list(max_skip.index)[0]:
+        print(' , ', end="")
+    print("{}-{}-{}".format(i[9:], max_skip[i],
+          round(max_skip[i]/len(df), 3)), end="")
+print("\n")
+# print the questions that are most wrongly answered
+false_ans = false_ans.sort_values(ascending=False)
+max_fault = false_ans[false_ans == false_ans.max()]
+print("Question that most people answer incorrectly: ", end="")
+for i in list(max_fault.index):
+    if i != list(max_fault.index)[0]:
+        print(' , ', end="")
+    print("{}-{}-{}".format(i[9:], max_fault[i],
+          round(max_fault[i]/len(df), 3)), end="")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,10 @@
-import regex as re
-import numpy as np
 import pandas as pd
-
-# list = re.split(',', '1,,2,3')
-# series = pd.Series([list])
-# print(series)
-
-data='
+# print("Mean (average) score: ", round(marks.mean(), 3))
+# print("Highest score: ", round(marks.max(), 3))
+# print("Lowest score: ", round(marks.low(), 3))
+# print("Range of scores: ", round(marks.max()-marks.min(), 3))
+# print("Median score: ", round(marks.median(), 3))
+idx = pd.Index(['Harry', 'Mike', 'Arther', 'Nick',
+                'Harry', 'Arther'], name='Student')
+ic = idx.value_counts()
+print(ic['a'])
